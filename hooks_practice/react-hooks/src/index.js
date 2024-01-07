def1ffc30123be1f658cf11a5c3c40ab2045c3ee
emmadickenson2 <emma.dickenson@wsu.edu>
2024-01-07 13:47:54 -0800

    useReducer for toggle

--- hooks_practice/react-hooks/src/index.js
+++ hooks_practice/react-hooks/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function App() {
-  const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber,
-     0 
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
   );
+
   return (
-    <h1 onClick={() => setNumber(1)}>{number}</h1>
+    <>
+      <input
+        type="checkbox"
+        value={checked} 
+        onChange={toggle}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
   )
 }
 
