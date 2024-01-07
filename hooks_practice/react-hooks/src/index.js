f5aca5798f0eb21cd3f27af670fcd139a40b2290
emmadickenson2 <emma.dickenson@wsu.edu>
2024-01-07 15:05:13 -0800

    practice with useRef

--- hooks_practice/react-hooks/src/index.js
+++ hooks_practice/react-hooks/src/index.js
@@ -1,22 +1,31 @@
-import React, {useReducer} from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+
 function App() {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const sound = useRef();
+  const color = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`${soundVal} sounds like ${colorVal}`)
+    sound.current.value="";
+    color.current.value=""
+  }
 
   return (
-    <>
-      <input
-        type="checkbox"
-        value={checked} 
-        onChange={toggle}
+    <form onSubmit={submit}>
+      <input 
+        ref={sound} 
+        type="text" 
+        placeholder="Sound..."
       />
-      {checked ? "checked" : "not checked"}
-    </>
+      <input ref={color} type="color"/>
+      <button>ADD</button>
+    </form>
   )
 }
 
