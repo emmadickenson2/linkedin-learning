cbcc2f3c3f1cfc05e9f038c1cd7ab658012da51e
emmadickenson2 <emma.dickenson@wsu.edu>
2024-01-09 10:46:27 -0800

    created custom hook

--- hooks_practice/react-hooks/src/index.js
+++ hooks_practice/react-hooks/src/index.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { useInput } from './useInput';
 
 
 function App() {
-  const [sound, setSound] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("")
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    alert(`${sound} sounds like ${color}`)
-    setSound("");
-    setColor("#000000");
+    alert(`${titleProps.value} sounds like ${colorProps.value}`)
+    resetTitle();
+    resetColor()
   }
 
   return (
     <form onSubmit={submit}>
       <input 
-        value={sound}
+        {...titleProps}
         type="text" 
         placeholder="Sound..."
-        onChange={(e) => setSound(e.target.value)}
       />
       <input 
-        value={color} 
+        {...colorProps} 
         type="color"
-        onChange={(e) => setColor(e.target.value)}/>
+      />
       <button>ADD</button>
     </form>
   )
