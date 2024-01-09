0769ddf92d30da490d0c9671f8b86cac118dd6c4
emmadickenson2 <emma.dickenson@wsu.edu>
2024-01-09 11:16:10 -0800

    practice adding data with useContext hook

--- hooks_practice/react-hooks/src/index.js
+++ hooks_practice/react-hooks/src/index.js
@@ -1,39 +1,20 @@
-import React, { useState } from 'react';
+import { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { useInput } from './useInput';
+import App from "./App"
 
+export const TreesContext = createContext()
 
-function App() {
-  const [titleProps, resetTitle] = useInput("")
-  const [colorProps, resetColor] = useInput("#000000");
-
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value} sounds like ${colorProps.value}`)
-    resetTitle();
-    resetColor()
-  }
-
-  return (
-    <form onSubmit={submit}>
-      <input 
-        {...titleProps}
-        type="text" 
-        placeholder="Sound..."
-      />
-      <input 
-        {...colorProps} 
-        type="color"
-      />
-      <button>ADD</button>
-    </form>
-  )
-}
+const trees = [
+  {id: "1", type: "Maple" },
+  {id: "2", type: "Oak" },
+  {id: "3", type: "Family" },
+  {id: "4", type: "Component" }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <TreesContext.Provider value={{ trees }}>
     <App />
-  </React.StrictMode>
+  </TreesContext.Provider>
 );
