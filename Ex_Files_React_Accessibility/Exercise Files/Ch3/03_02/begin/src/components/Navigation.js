2a2dd98d7d18ceac19312848c25ca8b0a5fb761a
emmadickenson2 <emma.dickenson@wsu.edu>
2024-01-09 18:51:42 -0800

    made nav dropdown disapear after click through

---
+++ Ex_Files_React_Accessibility/Exercise Files/Ch3/03_02/begin/src/components/Navigation.js
@@ -0,0 +1,66 @@
+import React from "react";
+import 'styles/navigation.css'
+import logo from "assets/images/logo-wordmark-color.svg"
+
+class Navigation extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isOpen: false
+    }
+
+    this.timeOutId = null;
+  }
+
+  onClickHandler = () => {
+    this.setState(currentState => ({
+      isOpen: !currentState.isOpen
+    }));
+  }
+
+  onBlurHandler = () => {
+    this.timeoutId = setTimeout(() => {
+      this.setState({
+        isOpen: false
+      })
+    })
+  }
+
+  onFocusHandler = () => {
+    clearTimeout(this.timeOutId)
+  }
+
+  render() {
+    return (
+      <div className="nav__container">
+        <a href="/">
+          <img src={logo} alt="logo" className="logo" />
+        </a>
+        <nav>
+          <div 
+            onFocus={this.onFocusHandler}
+            onBlus={this.onBlurHandler}>
+            <button onClick={this.onClickHandler} className="dropdown__button">
+              Browse
+              <i className="arrow down"></i>
+            </button>
+            {this.state.isOpen && (
+              <ul className="dropdown__options">
+                <li><a href="/oil-lovers-club">Oil Lover's Club</a></li>
+                <li><a href="/shop">Shop</a></li>
+                <li><a href="/blog">Blog</a></li>
+              </ul>
+            )}
+          </div>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/tours-events">Tours & Events</a>
+          <a href="/contact">Contact</a>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
